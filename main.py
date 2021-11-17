d44eae4b58633a6673260d832c64724c1627afd7
Ignacio Gonzalez <ignacio.gonzalez@pregrado.uoh.cl>
2021-11-17 17:24:33 -0300

    "Se añaden gatos con comando /sus"

--- main.py
+++ main.py
@@ -8,11 +8,24 @@ def get_url():
     url = contents['url']
     return url
 
+def get_url_2():
+    contents = requests.get('https://api.thecatapi.com/v1/images/search').json()[0]
+    url = contents['url']
+    return url
+
 def get_image_url():
     allowed_extension = ['jpg','jpeg','png']
     file_extension = ''
     while file_extension not in allowed_extension:
         url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
+def get_image_url_2():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url_2()
         file_extension = re.search("([^.]*)$",url).group(1).lower()
     return url
 
@@ -22,10 +35,16 @@ def bop(update, context):
     chat_id = update.message.chat_id
     context.bot.send_photo(chat_id=chat_id, photo=url)
 
+def sus(update, context):
+    url = get_image_url_2()
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id=chat_id, photo=url)
+
 def main():
     updater = Updater('2094169885:AAF9h9DQGaj_uE_F5E1lBTW-7QHNEMZwd1E', use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop',bop))
+    dp.add_handler(CommandHandler('bop', bop))
+    dp.add_handler(CommandHandler('sus', sus))
     updater.start_polling()
     updater.idle()
 
